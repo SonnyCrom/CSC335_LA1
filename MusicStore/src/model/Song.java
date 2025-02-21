c9f25ef86862a6b39f7c4129456631bd01f0bf62
SonnyCrom <166543703+SonnyCrom@users.noreply.github.com>
2025-02-21 12:14:52 -0700

    Update Song.java

--- MusicStore/src/model/Song.java
+++ MusicStore/src/model/Song.java
@@ -2,4 +2,37 @@ package model;
 
 public class Song {
 
+	String name;
+	String author;
+	String genre;
+	int year;
+	
+	
+	public Song(String name, String author, String genre, int year ) {
+		this.name = name;
+		this.author = author;
+		this.genre = genre;
+		this.year = year;
+	}
+	
+	// Returns a string of all the Songs information
+	public String getInfo() {
+		String info = this.name + " by " + this.author + ". Genre: " + this.genre + " Year: " + this.year;
+		return info;
+	}
+	
+	
+	// Basic Getters (No references, all primitive)
+	public String getName() {
+		return this.name;
+	}
+	public String getAuthor() {
+		return this.author;
+	}
+	public String getGenre() {
+		return this.genre;
+	}
+	public int getYear() {
+		return this.year;
+	}
 }
