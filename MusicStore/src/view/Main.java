93f5628b99846439bc53c9c81fec07c03897e105
SonnyCrom <ssteincrom@arizona.edu>
2025-02-21 13:37:35 -0700

    Test

---
+++ MusicStore/src/view/Main.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.LibraryModel;
+import model.MusicStore;
+
+public class Main {
+	public static void main(String[] args) {
+		MusicStore store = new MusicStore();
+		LibraryModel user = new LibraryModel();
+		
+		
+		
+	}
+}
