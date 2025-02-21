40b987055b06faac51a9cd422616b1d2a88ecca1
SonnyCrom <166543703+SonnyCrom@users.noreply.github.com>
2025-02-21 12:13:35 -0700

    Update Album.java

--- MusicStore/src/model/Album.java
+++ MusicStore/src/model/Album.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Album {
 
+	private ArrayList<Song> songs;
+	
+	
+	public Album() {
+		this.songs = new ArrayList<Song>();
+	}
+	
+	// Searches for a Song within album
+	public String searchSong(String name) {
+		for (int i = 0; i < songs.size(); i++) {
+			if (songs.get(i).getName().equals(name)){
+				return songs.get(i).getInfo();
+			}
+		}
+		return "";
+	}
 }
