2ed07ff3c12f1ed42c2942e7df61bc4f9e784042
SonnyCrom <166543703+SonnyCrom@users.noreply.github.com>
2025-02-21 12:14:20 -0700

    Update MusicStore.java

--- MusicStore/src/model/MusicStore.java
+++ MusicStore/src/model/MusicStore.java
@@ -1,5 +1,28 @@
+// Class - CSC 335 - LA1
+
+
 package model;
+import java.util.ArrayList;
+
+//import model.Song;
+
 
 public class MusicStore {
-
+	
+	private ArrayList<Album> albumList;
+	
+	
+	public MusicStore() {
+		this.albumList = new ArrayList<Album>();
+	}
+	
+	public String songInfo(String name) {
+		for (int i = 0; i < albumList.size(); i++) {
+			String songInformation = albumList.get(i).searchSong(name);
+			if (songInformation.length() != 0) {
+				return songInformation;
+			} 
+		}
+		return "Song not found!";
+	}
 }
