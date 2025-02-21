383f36974eeebb9b67881dfd546bc4f4cd79322f
SonnyCrom <166543703+SonnyCrom@users.noreply.github.com>
2025-02-21 12:54:27 -0700

    Update Album.java

--- MusicStore/src/model/Album.java
+++ MusicStore/src/model/Album.java
@@ -5,19 +5,42 @@ import java.util.ArrayList;
 public class Album {
 
 	private ArrayList<Song> songs;
+	private String name;
+	private String author;
+	private String genre;
+	private int year;
 	
-	
+	// Add way to read in files 
 	public Album() {
 		this.songs = new ArrayList<Song>();
+		this.name = "JOhn"; //Replace
 	}
 	
 	// Searches for a Song within album
-	public String searchSong(String name) {
+	public ArrayList<String> searchSong(String name) {
+		ArrayList<String> songList = new ArrayList<String>();
+		
+		// In case multiple same named songs in one Album
 		for (int i = 0; i < songs.size(); i++) {
 			if (songs.get(i).getName().equals(name)){
-				return songs.get(i).getInfo();
+				songList.add(songs.get(i).getInfo());
 			}
 		}
-		return "";
+		return songList;
+	}
+	
+	
+	//Basic Getters
+	public String getName() {
+		return this.name;
+	}
+	public String getInfo() {
+		String info = this.name + " by " + this.author + " Genre: " + this.genre + " Year: " + this.year;
+		return info;
+	}
+	public ArrayList<Song> getSongs() {
+		// Is this a escaping references?
+		ArrayList<Song> temp = (ArrayList<Song>) songs.clone();
+		return temp;
 	}
 }
