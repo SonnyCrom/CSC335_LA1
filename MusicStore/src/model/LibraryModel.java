667b855b81a6d3f9d8d8dab546d89b0343831b90
SonnyCrom <166543703+SonnyCrom@users.noreply.github.com>
2025-02-21 12:15:22 -0700

    Add files via upload

---
+++ MusicStore/src/model/LibraryModel.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.ArrayList;
+import model.Song;
+
+public class LibraryModel {
+	private ArrayList<Song> songList; 
+	private ArrayList<ArrayList<Song>> playList;
+	
+	public LibraryModel() {
+		this.songList = new ArrayList<Song>();
+		this.playList = new ArrayList<ArrayList<Song>>();
+		playList.add(songList);
+	}
+	
+	
+	
+}
